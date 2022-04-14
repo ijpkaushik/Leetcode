d5e1b3c306511f2544c81e3a9ba2f592eccd333e
Jai Prakash Kaushik <78019107+ijpkaushik@users.noreply.github.com>
2022-04-14 12:03:44 +0530

    Added solution - LeetHub

---
+++ Delete nodes having greater value on right - GFG/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,111 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+ // } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    Node* reverse(Node *a){
+      Node *t1 = NULL, *curr = a;
+      while(curr){
+          Node *t2 = curr->next;
+          curr->next = t1;
+          t1 = curr;
+          curr = t2;
+      }
+        return t1;
+    }
+    Node *compute(Node *head)
+    {
+       Node* temp=reverse(head);
+       head=temp;
+      while(temp->next!=NULL){
+          if(temp->next->data<temp->data){
+            //   Node *del = temp;
+            //   delete(del);
+              temp->next=temp->next->next;
+          }
+          else{
+              temp=temp->next;
+          }
+      }
+       return reverse(head);
+    }
+    
+};
+   
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+  // } Driver Code Ends

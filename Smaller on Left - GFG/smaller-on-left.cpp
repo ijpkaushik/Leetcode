a6e85ded7b4f587d6d953577f441d56cf2c03d4b
Jai Prakash Kaushik <78019107+ijpkaushik@users.noreply.github.com>
2022-04-24 00:31:44 +0530

    Added solution - LeetHub

---
+++ Smaller on Left - GFG/smaller-on-left.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> Smallestonleft(int arr[], int n);
+
+
+int main() {
+	
+	int t;
+	
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n;
+	    cin >> n;
+	    int arr[n+1];
+	    
+	    for(int i=0;i<n;i++){
+	        cin>> arr[i];
+	    }
+	    
+	    vector<int> b;
+	    b = Smallestonleft(arr, n);
+	    for(int i=0;i<n;i++)
+	        cout << b[i] << " ";
+	       
+	    cout << endl;
+	    
+	    
+	}
+	
+	return 0;
+}// } Driver Code Ends
+
+
+vector<int> Smallestonleft(int arr[], int n)
+{
+    vector<int> ans;
+    set<int> s;
+    s.insert(-1);
+    for(int i=0; i<n; i++){
+        auto itr=s.lower_bound(arr[i]);
+        itr--;
+        s.insert(arr[i]);
+        ans.push_back(*itr);
+    }
+    
+    return ans;
+}

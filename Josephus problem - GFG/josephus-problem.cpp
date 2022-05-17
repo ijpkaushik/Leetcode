f8982719d6d7ba1f185cfce7a47524bb4c0a32bd
Jai Prakash Kaushik <78019107+ijpkaushik@users.noreply.github.com>
2022-05-17 19:05:57 +0530

    Added solution - LeetHub

---
+++ Josephus problem - GFG/josephus-problem.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution
+{
+    public:
+    int josephus(int n, int k) {
+        int res=0;
+        for(int i=1;i<=n;i++){
+            res=(res+k)%i;
+        }
+        return res+1;
+    }
+};
+
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;//taking input n and k
+		
+		//calling josephus() function
+		Solution ob;
+		cout<<ob.josephus(n,k)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends

bba560f13a959909639ed4b73891789fe3985231
Jai Prakash Kaushik <78019107+ijpkaushik@users.noreply.github.com>
2022-05-21 23:04:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.06%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,17 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        // for(int i=1; i<=n; i++){
-        //     if(isBadVersion(i))
-        //         return i;
-        // }
-        // return -1;
-        int low = 1, high = n, mid;
-        while (low < high) {
-            mid = low + (high - low) / 2;
-            if (isBadVersion(mid)) high = mid;
-            else low = mid+1;
+        int low=1, high=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isBadVersion(mid)==true && isBadVersion(mid-1)==false)
+                return mid;
+            if(isBadVersion(mid))
+                high=mid-1;
+            if(!isBadVersion(mid))
+                low=mid+1;                
         }
-        return low;
+        return -1;
     }
 };

7a8ce132f2524634f22c8b1ee889dc34cacd5ae7
Jai Prakash Kaushik <78019107+ijpkaushik@users.noreply.github.com>
2022-05-23 11:35:49 +0530

    Time: 5 ms (16.52%), Space: 6.2 MB (56.84%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans="";
+        int i=0;
+        while(i<command.size()){
+            if(command[i]=='G'){                
+                ans+='G';
+                i++;
+            }
+            else if(command[i]=='(')
+                if(command[i+1]==')'){
+                    ans+='o';
+                    i+=2;
+                }
+                else{
+                    ans+="al";
+                    i=i+4;
+                }                    
+        }
+        return ans;
+    }
+};

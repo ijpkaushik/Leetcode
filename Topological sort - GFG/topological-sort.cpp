2bbf2ccd5927d2a5cd08abbc0c8d72be04ee8a43
Jai Prakash Kaushik <78019107+ijpkaushik@users.noreply.github.com>
2022-06-28 14:27:04 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order.
+	void dfs(int i, bool vis[], stack<int> &s, vector<int> adj[]){
+	    vis[i]=true;
+	    for(auto itr: adj[i])
+	        if(!vis[itr])
+	            dfs(itr, vis, s, adj);
+	    s.push(i);
+	}
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    bool vis[V];
+	    memset(vis, false, sizeof(vis));
+	    vector<int> ans;
+
+        stack<int> s;
+	    
+	   for(int i=0; i<V; i++)
+	        if(!vis[i])
+	            dfs(i, vis, s, adj);
+	            
+	   while(!s.empty()){
+	       int num=s.top();
+	       s.pop();
+	       ans.push_back(num);
+	   }
+	   return ans;
+	}
+};
+
+// { Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}  // } Driver Code Ends
